500d7fbb3c630785cf0ab4bae279dc10d110b92f
jingwen-wu0601 <61522389+jingwen-wu0601@users.noreply.github.com>
2020-05-13 14:45:22 -0700

    search name function add, css update

--- components/receipt/main2.js
+++ components/receipt/main2.js
@@ -3,6 +3,9 @@ var newReceiptTable = new Receipt(myReceiptTable);
 
 var myRandomForm = document.getElementById("getRandomReceipt");
 var newRandomForm = new RandomForm(myRandomForm);
-var newRandomSearch = new RandomMenu(newReceiptTable, newRandomForm);
 
-newRandomSearch.start();
+var myIngeredientForm = document.getElementById("ingredientSearch");
+var newIngeredientForm = new IngredientForm(myIngeredientForm);
+
+var newSearch = new Information(newReceiptTable, newRandomForm, newIngeredientForm);
+newSearch.start();
---
+++ components/receipt/receipt-modal.js
@@ -0,0 +1,128 @@
+class Receipt {
+  constructor(tableElement) {
+    this.tableElement = tableElement;
+  }
+
+  updateTable(data) {
+    document.getElementById("modalOverlay").classList.remove("hidden");
+    var titleElement = document.querySelector(".modal-title");
+    var createTitle = document.createElement("h5");
+    createTitle.textContent = data.recipes[0].title;
+    titleElement.appendChild(createTitle);
+    var bodyElement = document.querySelector("#modal-body");
+    bodyElement.className = "random-modal-body";
+    var imgElement = document.createElement("div");
+    imgElement.setAttribute("style", "background-image: url(" + data.recipes[0].image + ");");
+    imgElement.setAttribute("id", "modalImage");
+    imgElement.className = "col-4 modalImage";
+    var rightColumn = document.createElement("div");
+    rightColumn.className = "rightColumn col-7";
+    bodyElement.append(imgElement, rightColumn);
+
+    var cuisineTitle = document.createElement("h6");
+    var cusines = data.recipes[0].cuisines;
+    var cusinesName = "";
+    for (let i = 0; i < cusines.length; i++) {
+      cusinesName += cusines[i] + " ";
+    }
+    if (cusines.length !== 0) {
+      cuisineTitle.textContent = "Cuisines: " + cusinesName;
+      rightColumn.appendChild(cuisineTitle);
+    }
+
+    var summaryTitle = document.createElement("h6");
+    summaryTitle.textContent = "Summary";
+    var summaryElement = document.createElement("div");
+    summaryElement.innerHTML = data.recipes[0].summary;
+    var cookingTime = document.createElement("h6");
+    cookingTime.textContent = "Ready in Minutes: " + data.recipes[0].readyInMinutes + " min.";
+    var serveSize = document.createElement("h6");
+    serveSize.textContent = "Servings: " + data.recipes[0].servings;
+    rightColumn.append(summaryTitle, summaryElement, cookingTime, serveSize);
+
+    var ingredientsTitle = document.createElement("h6");
+    ingredientsTitle.textContent = "Ingresients";
+    rightColumn.appendChild(ingredientsTitle);
+    var extendedIngredients = data.recipes[0].extendedIngredients;
+    for (let i = 0; i < extendedIngredients.length; i++) {
+      var ingredients = document.createElement("p");
+      ingredients.textContent = extendedIngredients[i].original;
+      rightColumn.appendChild(ingredients);
+    }
+
+    var instructionTitle = document.createElement("h6");
+    instructionTitle.textContent = "Instruction";
+    var instructionElement = document.createElement("div");
+    instructionElement.innerHTML = data.recipes[0].instructions;
+    rightColumn.append(instructionTitle, instructionElement);
+
+
+    document.querySelector("#closeButton").addEventListener("click", function () {
+      document.getElementById("modalOverlay").className += " hidden";
+      document.querySelector(".modal-title").innerHTML = "";
+      document.querySelector("#modal-body").innerHTML = "";
+    })
+  }
+
+  updateByIngredient(data) {
+    document.getElementById("modalOverlay").classList.remove("hidden");
+    for (let i = 0; i < data.length; i++) {
+      var bodyElement = document.querySelector("#modal-body");
+      bodyElement.className = "modal-body"
+      var rowElement = document.createElement("div");
+      rowElement.className = "modal-row";
+      bodyElement.appendChild(rowElement);
+
+      var titleElement = document.createElement("div");
+      titleElement.className = "modal-title";
+      var createTitle = document.createElement("h5");
+      createTitle.textContent = data[i].title;
+      titleElement.appendChild(createTitle);
+      rowElement.appendChild(titleElement);
+
+      var content = document.createElement("div");
+      content.className = "modal-result";
+      rowElement.appendChild(content);
+
+      var imgElement = document.createElement("div");
+      imgElement.setAttribute("style", "background-image: url(" + data[i].image + ");");
+      imgElement.setAttribute("id", "modalImage");
+      imgElement.className = "col-4 modalImage";
+      var rightColumn = document.createElement("div");
+      rightColumn.className = "rightColumn col-5";
+      content.append(imgElement, rightColumn);
+
+      var usedTitle = document.createElement("h6");
+      usedTitle.textContent = "Essencial Ingredients";
+      rightColumn.appendChild(usedTitle);
+      var useArry = data[i].usedIngredients;
+      if (useArry.length === 0) {
+        var none = document.createElement("p");
+        none.textContent = "None";
+        rightColumn.appendChild(none);
+      } else {
+        for (let n = 0; n < useArry.length; n++) {
+          var ingredients = document.createElement("p");
+          ingredients.textContent = useArry[n].originalString;
+          rightColumn.appendChild(ingredients);
+        }
+      }
+
+      var missingTitle = document.createElement("h6");
+      missingTitle.textContent = "Additional Ingredients Needed";
+      rightColumn.appendChild(missingTitle);
+      var ingArry = data[i].missedIngredients;
+      for (let n = 0; n < ingArry.length; n++) {
+        var ingredients = document.createElement("p");
+        ingredients.textContent = ingArry[n].originalString;
+        rightColumn.appendChild(ingredients);
+      }
+    }
+
+    document.querySelector("#closeButton").addEventListener("click", function () {
+      document.getElementById("modalOverlay").className += " hidden";
+      document.querySelector(".modal-title").innerHTML = "";
+      document.querySelector("#modal-body").innerHTML = "";
+    })
+  }
+}
