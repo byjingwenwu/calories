bf9148c8229e5be981541eb9ce99c085564d5c8a
Jingwen Wu <wuesperanza@gmail.com>
2020-10-22 16:04:37 -0700

    update

--- components/restaurant/search-location-form.js
+++ components/restaurant/search-location-form.js
@@ -1,5 +1,5 @@
 class SearchForm {
-  constructor (formElement) {
+  constructor(formElement) {
     this.formElement = formElement;
     this.handleSearch = this.handleSearch.bind(this);
     this.formElement.addEventListener("submit", this.handleSearch);
@@ -13,7 +13,27 @@ class SearchForm {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     var location = formData.get("search-restaurant");
-    this.searchByLocation(location);
-    event.target.reset();
+    if (!location) {
+      var modal = document.getElementById("restaurantModal")
+      modal.classList.add("d-block")
+      var errorTitle = document.getElementById("modalTitle")
+      errorTitle.textContent = "Error"
+      var errorBody = document.getElementById("modal-body")
+      var error = document.createElement("h6")
+      error.className = "theme-color col-12 mt-4"
+      error.innerHTML = "Please enter an valid zipcode or city name."
+      errorBody.append(error)
+      document.querySelectorAll("#controlCloseModal").forEach(function () {
+        addEventListener("click", function () {
+          modal.classList.remove("d-block")
+          errorBody.innerHTML = "";
+          errorTitle.textContent = ""
+        })
+      })
+      event.stopPropagation();
+    } else {
+      this.searchByLocation(location);
+      event.target.reset();
+    }
   }
 }
